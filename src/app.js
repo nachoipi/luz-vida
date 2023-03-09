90919c590fa5d4cc334d206db875fd9167056cc4
unknown <ipinazarignacio@gmail.com>
2023-03-09 08:28:20 -0300

    EJS Template engine installed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+/*TEMPLATE CONFIG*/
+app.set('views', join(__dirname,'./views'));
+app.set('view engine','ejs')
+
 /*PORT CONFIG*/
 const {port,start} = require('./modules/server')
 app.listen(port,start())
@@ -10,10 +14,8 @@ const {join} = require('path');
 const statics = require('./modules/static');
 app.use(statics(join(__dirname,'../public')));
 
-/*ROUTES*/
-let rutasProductos = require('./routes/productos.routes.js')
 let rutasMain = require('./routes/main.js')
 
-/*USE*/
+/*ROUTES*/
 app.use(require('./routes/productos.routes'));
 app.use('/', rutasMain);
